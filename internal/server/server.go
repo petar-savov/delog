9c98d06515f43f728ce9efc0508a390a4c052f3a
petar-savov <p.savov@outlook.com>
2023-02-27 21:11:29 +0000

    Add service discovery and load balancing

--- internal/server/server.go
+++ internal/server/server.go
@@ -25,8 +25,9 @@ import (
 )
 
 type Config struct {
-	CommitLog  CommitLog
-	Authorizer Authorizer
+	CommitLog   CommitLog
+	Authorizer  Authorizer
+	GetServerer GetServerer
 }
 
 const (
@@ -196,3 +197,15 @@ func subject(ctx context.Context) string {
 }
 
 type subjectContextKey struct{}
+
+func (s *grpcServer) GetServers(ctx context.Context, req *api.GetServersRequest) (*api.GetServersResponse, error) {
+	servers, err := s.GetServerer.GetServers()
+	if err != nil {
+		return nil, err
+	}
+	return &api.GetServersResponse{Servers: servers}, nil
+}
+
+type GetServerer interface {
+	GetServers() ([]*api.Server, error)
+}
