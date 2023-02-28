789e6181ac39d57539ee7fd2c0f31ad30a9171fe
petar-savov <p.savov@outlook.com>
2023-02-28 21:51:31 +0000

    Refactor: WithInsecure is deprecated

--- cmd/getservers/main.go
+++ cmd/getservers/main.go
@@ -8,12 +8,13 @@ import (
 
 	api "github.com/petar-savov/delog/api/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	addr := flag.String("addr", ":8400", "service address")
 	flag.Parse()
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal(err)
 	}
